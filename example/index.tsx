f9cdfca9341dd2fe2e0a91eec359bb1ce0159a5a
usersina <benna.anis2@gmail.com>
2023-09-24 21:53:13 +0100

    add base working example

--- example/index.tsx
+++ example/index.tsx
@@ -1,12 +1,41 @@
 import 'react-app-polyfill/ie11';
+
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { Thing } from '../.';
+import { AsyncTypewriter, getIterableStream } from '../.';
 
 const App = () => {
+  const [stream, setStream] = React.useState<AsyncIterable<string> | null>(
+    null
+  );
+
+  React.useEffect(() => {
+    const fetchStream = async () => {
+      const response = await fetch(
+        'http://localhost:5000/stream?chunks_amount=50'
+      );
+
+      if (response.status !== 200) {
+        console.error('Request failed');
+        return;
+      }
+
+      if (!response.body) {
+        console.error('Request returned no body');
+        return;
+      }
+
+      const stream = getIterableStream(response.body);
+      setStream(stream);
+    };
+
+    fetchStream();
+  }, []);
+
   return (
     <div>
-      <Thing />
+      <h1>AsyncTypewriter</h1>
+      {stream && <AsyncTypewriter stream={stream} />}
     </div>
   );
 };
